a697e7b80503eaaa938fe6c76c62894e89858b8e
spurti-chopra <spurti@us.ibm.com>
2018-02-11 07:42:53 +0000

    puts statements for debug

--- cookbooks/travis_build_environment/recipes/python.rb
+++ cookbooks/travis_build_environment/recipes/python.rb
@@ -136,10 +136,10 @@ node['travis_build_environment']['pythons'].each do |py|
 
   packages = []
 
-  #puts  node['travis_build_environment']['python_aliases']
+  puts  node['travis_build_environment']['python_aliases']
   node['travis_build_environment']['python_aliases'].fetch(py, []).concat(['default', py]).each do |name|
     puts name
-    #puts node['travis_build_environment']['pip']['packages']
+    puts node['travis_build_environment']['pip']['packages']
     puts node['travis_build_environment']['pip']['packages'].fetch(name, [])
     packages.concat(node['travis_build_environment']['pip']['packages'].fetch(name, []))
     puts "----------------------------------------------1"
